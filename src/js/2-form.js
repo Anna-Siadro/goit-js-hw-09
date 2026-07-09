6d66ab11bf243735ec8d0b09654b41661370762e
Anna Siadro <anna_anna_111@ukr.net>
2026-07-09 17:07:51 +0300

    first-fix

---
+++ src/js/2-form.js
@@ -0,0 +1,48 @@
+import '../css/form.css';
+
+const form = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+
+const formData = {
+  email: '',
+  message: '',
+};
+
+const savedData = localStorage.getItem(STORAGE_KEY);
+
+if (savedData) {
+  const parsedData = JSON.parse(savedData);
+
+  formData.email = parsedData.email || '';
+  formData.message = parsedData.message || '';
+
+  form.elements.email.value = formData.email;
+  form.elements.message.value = formData.message;
+}
+
+form.addEventListener('input', event => {
+  const field = event.target.name;
+  const value = event.target.value.trim();
+
+  formData[field] = value;
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  if (formData.email === '' || formData.message === '') {
+    alert('Plese fill out the form');
+    return;
+  }
+
+  console.log(formData);
+
+  localStorage.removeItem(STORAGE_KEY);
+
+  form.requestFullscreen();
+
+  formData.email = '';
+  formData.message = '';
+});
